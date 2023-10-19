448fdd003b4f7fed2342b8d9135f19a162c3069f
NORZACO <caspermotive4@gmail.com>
2023-10-19 20:51:52 +0200

    mv to window #mv02

--- clientside/src/pages/dashBoard/MainDashboardRoute.jsx
+++ clientside/src/pages/dashBoard/MainDashboardRoute.jsx
@@ -4,21 +4,31 @@ import DashboardLayer from "./DashboardLayer";
 import AuthUserPage from "./AuthUserPage";
 
 import Auth_group_permissionsPage from "./auth_group_permissions";
+import Auth_permissionPage from "./auth_permissionPage";
+import Auth_userGroupPage from "./auth_user_groupsPage";
+import Bloge_categoryPage from "./bloge_categoryPage";
+import Bloge_postPage from "./bloge_postPage";
+import Bloge_post_categoryPage from "./bloge_post_categoryPage";
+import Super_admin_logPage from "./super_admin_logPage";
+import Super_content_typesPage from "./super_content_typesPage";
 
 export default function MainDashboardRoute() {
-  return <Route path="dashboard" element={<DashboardLayer />}>
-    <Route index element={<AlbumPage />} />
-    <Route path="authuser" element={<AuthUserPage />} />
-    <Route path="auth_group_permissions" element={<Auth_group_permissionsPage />} />
-    <Route path="auth_permission" element={<h1> auth_permission</h1>} />
-    <Route path="auth_user" element={<h1> auth_user</h1>} />
-    <Route path="auth_user_groups" element={<h1> auth_user_groups</h1>} />
-    <Route path="bloge_category" element={<h1> bloge_category</h1>} />
-    <Route path="bloge_post" element={<h1> bloge_post</h1>} />
-    <Route path="bloge_post_category" element={<h1> bloge_post_category</h1>} />
-    <Route path="super_admin_log" element={<h1> super_admin_log</h1>} />
-    <Route path="super_content_types" element={<h1> super_content_types</h1>} />
-  </Route>;
+  return (
+    <Route path="dashboard" element={<DashboardLayer />}>
+      <Route index element={<AlbumPage />} />
+      <Route path="authuser" element={<AuthUserPage />} />
+      <Route
+        path="auth_group_permissions"
+        element={<Auth_group_permissionsPage />}
+      />
+      <Route path="auth_permission" element={<Auth_permissionPage />} />
+      <Route path="auth_user" element={<AuthUserPage />} />
+      <Route path="auth_user_groups" element={<Auth_userGroupPage />} />
+      <Route path="bloge_category" element={<Bloge_categoryPage />} />
+      <Route path="bloge_post" element={<Bloge_postPage />} />
+      <Route path="bloge_post_category" element={<Bloge_post_categoryPage />} />
+      <Route path="super_admin_log" element={<Super_admin_logPage />} />
+      <Route path="super_content_types" element={<Super_content_typesPage />} />
+    </Route>
+  );
 }
-
-
---
+++ clientside/src/pages/dashBoard/auth_permissionPage.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+export default function auth_permissionPage() {
+  return (
+    <div>
+        <h1>auth_permissionPage</h1>
+      
+    </div>
+  )
+}
